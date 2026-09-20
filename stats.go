d9bd98d43d5977b5f61e0234c7cc2d7fded7f7d3
maxsupermanhd <q3.max.2011@ya.ru>
2026-09-20 17:47:07 +0300

    reduce cache times even further

--- stats.go
+++ stats.go
@@ -16,7 +16,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var cachedStatsTables = caches.NewValueRefresh(wb, 15*time.Minute, func() ([]frontend.StatsTable, error) {
+var cachedStatsTables = caches.NewValueRefresh(wb, 5*time.Minute, func() ([]frontend.StatsTable, error) {
 	return collectStatsTables(context.Background())
 })
 
